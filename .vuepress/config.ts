4c88dc24aa88616b20a7f0f8be0beba4fd6beffc
xkrivzooh <xkrivzooh@gmail.com>
2022-09-15 11:14:53 +0800

    增加百度统计
    增加百度统计

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -6,6 +6,16 @@ export default defineUserConfig({
   title: "被遗忘的博客",
   description: "被遗忘的博客, 记录一些学习记录、编程知识、架构设计、职场工作等",
 
+  head: [['script', {}, `
+  var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?92056b950ab8ccf389c1ae384bf62091";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();
+  `]],
+
   base: "/",
 
   theme,
